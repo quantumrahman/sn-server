d855e734c2aba58f3e25d3f04c553d8825cc55d0
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-24 22:12:27 +0600

    feat: implement create services business logic

--- src/services/facility.services.js
+++ src/services/facility.services.js
@@ -1,5 +1,20 @@
-export const createService = async () => {
-    console.log('create facility');
+import Facility from '../models/facility.model.js';
+import AppError from '../utils/app.error.js';
+
+export const createService = async (payload) => {
+    const facilityExists = await Facility.findOne({ name: payload.name });
+
+    if (facilityExists) {
+        throw new AppError(400, 'Facility already exists');
+    }
+
+    return await Facility.create({
+        ...payload,
+        available_slots: payload.available_slots
+            .split(',')
+            .map((slot) => slot.trim())
+            .filter(Boolean),
+    });
 };
 
 export const readsService = async () => {
