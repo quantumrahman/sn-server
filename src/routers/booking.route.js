8889afd7e8c98eb9c8e9aacde729de0e0c375b0c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-24 20:50:52 +0600

    feat: implement controllers in routes

--- src/routers/booking.route.js
+++ src/routers/booking.route.js
@@ -2,32 +2,19 @@ import { Router } from 'express';
 
 const bookingRoute = Router();
 
-bookingRoute.route('/bookings').post((req, res) =>
-    res.status(200).json({
-        success: true,
-        message: 'POST -> Create booking',
-    })
-);
+import {
+    createController,
+    readsController,
+    readController,
+    updateController,
+} from '../controllers/booking.controllers.js';
 
-bookingRoute.route('/bookings').get((req, res) =>
-    res.status(200).json({
-        success: true,
-        message: 'GET -> Reads bookings',
-    })
-);
+bookingRoute.route('/bookings').post(createController);
 
-bookingRoute.route('/bookings/:id').get((req, res) =>
-    res.status({
-        success: true,
-        message: 'GET -> Read booking',
-    })
-);
+bookingRoute.route('/bookings').get(readsController);
 
-bookingRoute.route('/bookings/:id').patch((req, res) =>
-    res.status(200).json({
-        success: true,
-        message: 'PATCH -> Update booking',
-    })
-);
+bookingRoute.route('/bookings/:id').get(readController);
+
+bookingRoute.route('/bookings/:id').patch(updateController);
 
 export default bookingRoute;
