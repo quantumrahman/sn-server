1e903c1ab11355fb67d104c970c4a7ea0c7da0c4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-24 21:34:31 +0600

    feat: implement booking services function

---
+++ src/services/booking.services.js
@@ -0,0 +1,15 @@
+export const createService = async () => {
+    console.log('create booking');
+};
+
+export const readsService = async () => {
+    console.log('reads bookings');
+};
+
+export const readService = async () => {
+    console.log('read service');
+};
+
+export const updateService = async () => {
+    console.log('update service');
+};
