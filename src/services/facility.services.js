70bec16517151e2f0040ccfd540799e80c6ffe1b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-06-24 21:32:03 +0600

    feat: implement facility services function

---
+++ src/services/facility.services.js
@@ -0,0 +1,19 @@
+export const createService = async () => {
+    console.log('create facility');
+};
+
+export const readsService = async () => {
+    console.log('reads facility');
+};
+
+export const readService = async () => {
+    console.log('read service');
+};
+
+export const updateService = async () => {
+    console.log('update service');
+};
+
+export const deleteService = async () => {
+    console.log('delete service');
+};
